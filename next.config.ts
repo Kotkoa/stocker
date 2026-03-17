e023e42b0fca19a1b955cb3011d6cb338db2ae74
Kotkoa <kotkoa@gmail.com>
2026-03-17 17:19:00 +0100

    fix: update basePath configuration for GitHub Pages compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
-const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isGithubPages ? "/stocker" : "",
+  basePath,
   reactCompiler: true,
   images: {
     loader: "custom",
